29236256c92072548c77e76b1e04b2f63d10044e
tchindjeeric61 <tchindjeeric61@gmail.com>
2024-03-27 06:17:53 +0100

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,9 @@ import { Toaster } from "react-hot-toast";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Ricardo | Personal Portfolio",
-  description: "Ricardo is a full-stack developer with 8 years of experience.",
+  title: "Tchindje Eric | Personal Portfolio",
+  description:
+    "Eric is a full-stack developer with 3 years of experience and also junior data scientist.",
 };
 
 export default function RootLayout({
